ff26f3f35cdbe837373294737b1333ac0b1f123a
yoonsy <yegyo95@gmail.com>
2019-12-13 16:45:58 +0900

    translate.py

---
+++ telegram-bot/translate.py
@@ -0,0 +1,14 @@
+import requests
+from decouple import config
+
+url='https://translation.googleapis.com/language/translate/v2'
+key=config('GOOGLE_TOKEN')
+data={
+    'q': '엄마 판다는 새끼가 있네',
+    'source': 'ko',
+    'target': 'en'
+}
+result=requests.post(f'{url}?key={key}',data).json()
+print(result)
+
+# result.get('data').get('translations')[0].get(translatedText)
